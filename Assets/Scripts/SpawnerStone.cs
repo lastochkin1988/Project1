15aa0921444b3818754b4f1471513304afcf4d27
i.lastochkin@carx-tech.com <i.lastochkin@carx-tech.com>
2025-04-22 10:25:12 +0300

    Golf_commit_unit_4.1_03

---
+++ Assets/Scripts/SpawnerStone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Golf
+{
+    public class SpawnerStone : MonoBehaviour
+    {
+        public GameObject[] prefabs;
+
+        public void Spawn()
+        {
+            var prifab = GetRandomPrefab();
+
+            if (prifab == null )
+            {
+                return;
+            }
+            Instantiate(prifab, transform.position, Quaternion.identity);
+        }
+        private GameObject GetRandomPrefab()
+        {
+            if (prefabs.Length == 0)
+            {
+                return null;
+            }
+            
+            int index = Random .Range(0, prefabs.Length);
+            return prefabs[index];
+        }
+    }
+}
